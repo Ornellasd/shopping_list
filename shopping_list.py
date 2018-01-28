fc3de9b4383ca872cdebf1082bbfc4dc06f2f8ef
David Ornellast <ornellasd@gmail.com>
2018-01-27 16:13:34 -0800

    refactors code, fixes json dump io

--- shopping_list.py
+++ shopping_list.py
@@ -1,31 +1,39 @@
 import json
 import sys
 
-shopping_list = {}
 filename = 'shopping_list.json'
+shopping_list = {}
 
-while True:
+def print_list(items_dict, left_width, right_width):
+    print('Shopping List'.center(left_width + right_width, '-'))
+    for k, v in items_dict.items():
+        print(k.ljust(left_width, '.') + str(v).rjust(right_width))
 
-#    if filename:
-#        with open(filename, 'r') as f_obj:
-#            datastore = json.loads(filename.read())
-#            for k, v in datastore.items():
-#                print(k + ' ' + v)    
-     
-    list_item = input('What item would you like to add?: ').title()
-    qty_item = input('How much? (enter "q" to exit at any time): ').title()
-    
-    if list_item == 'Q' or qty_item == 'Q':
-        sys.exit()
-    
-    def print_list(items_dict, left_width, right_width):
-        print('Shopping List'.center(left_width + right_width, '-'))
-        for k, v in items_dict.items():
-            print(k.ljust(left_width, '.') + str(v).rjust(right_width))
-    
-    shopping_list[list_item] = qty_item
-    print_list(shopping_list, 20, 6)
-            
-    with open(filename, 'a') as f_obj:
-        json.dump(shopping_list, f_obj)
-        #print('List saved to "shopping_list.json"')
+def add_items():
+    while True:
+        list_item = input('What item would you like to add?: ').title()
+        if list_item == 'Q':
+            sys.exit()
+        qty_item = input('How much? (enter "q" to exit at any time): ').title()
+        if qty_item == 'Q':
+            sys.exit()
+        shopping_list[list_item] = qty_item
+        print_list(shopping_list, 20, 6)
+        with open(filename, 'w') as f:
+            json.dump(shopping_list, f)
+
+try:
+    with open(filename, 'r') as f:
+        shopping_list = json.load(f)
+        print_list(shopping_list, 20, 6)
+        alter_list = input('Would you like to add anything else?: ').title()
+        if alter_list == 'Y':
+            add_items()
+        elif alter_list == 'N':
+            sys.exit()
+except FileNotFoundError:
+    print("No existing shopping list...")
+    add_items()
+
+
+
